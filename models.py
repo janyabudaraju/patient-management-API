19cf79249ddea1a759575dca343ec4ef0acfa934
Janya Budaraju <budaraju.janya@gmail.com>
2024-02-01 15:32:35 -0500

    adding blood type

--- models.py
+++ models.py
@@ -14,14 +14,22 @@ class Patient(Base):
     last_name = Column(String(100), nullable=False)
     date_of_birth = Column(Date, nullable=False)
     sex = Column(String(50), nullable=False)
+    blood_type = Column(String(3), nullable=False)
     visits: Mapped[List["HospitalVisit"]] = relationship("HospitalVisit", back_populates="patient")
 
     @validates('sex')
     def validate_sex(self, key, sex):
         valid_sexes = ['Male', 'Female', 'Other']
         if sex not in valid_sexes:
-            raise ValueError("Invalid sex")
+            raise ValueError(f"Invalid sex: {sex}")
         return sex
+    
+    @validates('blood_type')
+    def validate_blood_type(self, key, blood_type):
+        valid_types = ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-']
+        if blood_type not in valid_types:
+            raise ValueError(f"Invalid blood type: {blood_type}")
+        return blood_type
 
 class HospitalVisit(Base):
     visit_id = Column(Integer, primary_key=True)
