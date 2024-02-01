dede6a4149bf45d7e95ec7868aaabd2f662d6f6b
Janya Budaraju <budaraju.janya@gmail.com>
2024-02-01 15:14:15 -0500

    minor bug fixes

--- models.py
+++ models.py
@@ -10,22 +10,22 @@ class Patient(Base):
     __tablename__ = 'patients'
     patient_id = Column(Integer, primary_key=True)
     first_name = Column(String(100), nullable=False)
-    middle_name = Column(String(100), nullable=True)
+    middle_name = Column(String(100))
     last_name = Column(String(100), nullable=False)
     date_of_birth = Column(Date, nullable=False)
     sex = Column(String(50), nullable=False)
     visits: Mapped[List["HospitalVisit"]] = relationship("HospitalVisit", back_populates="patient")
+
+    @validates('sex')
+    def validate_sex(self, key, sex):
+        valid_sexes = ['Male', 'Female', 'Other']
+        if sex not in valid_sexes:
+            raise ValueError("Invalid sex")
+        return sex
 
 class HospitalVisit(Base):
     visit_id = Column(Integer, primary_key=True)
     visit_cause = Column(Text)
     visit_date = Column(Date, nullable=False)
     patient: Mapped["Patient"] = relationship(back_populates="visits")
-    patient_id: Column(Integer, ForeignKey('patients.id'))
-
-@validates('sex')
-def validate_sex(self, key, sex):
-    valid_sexes = ['Male', 'Female', 'Other']
-    if sex not in valid_sexes:
-        raise ValueError("Invalid sex")
-    return sex
+    patient_id: Column(Integer, ForeignKey('patients.patient_id'))
