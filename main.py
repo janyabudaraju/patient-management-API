4e4355a4682ed960fbffc8ceb25741b0f8e772f4
Janya Budaraju <budaraju.janya@gmail.com>
2024-02-03 15:32:56 -0500

    adding get functionality

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import models
 import schemas
 from typing import List
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException, Depends, status
 from database import Base, engine, SessionLocal
 from sqlalchemy.orm import Session
 
@@ -19,15 +19,15 @@ app = FastAPI()
 async def root():
     return {"message": "Janya's REST API prototype!!"}
 
-@app.post("/patients/", response_model=schemas.PatientCreate)
+@app.post("/patients/", response_model = schemas.Patient, status_code = status.HTTP_201_CREATED)
 async def create_patient(patient_data: schemas.PatientCreate, db: Session = Depends(get_session)):
     db_patient = models.Patient(
-        first_name=patient_data.first_name,
-        middle_name=patient_data.middle_name,
-        last_name=patient_data.last_name,
-        date_of_birth=patient_data.date_of_birth,
-        sex=patient_data.sex,
-        blood_type=patient_data.blood_type
+        first_name = patient_data.first_name,
+        middle_name = patient_data.middle_name,
+        last_name = patient_data.last_name,
+        date_of_birth = patient_data.date_of_birth,
+        sex = patient_data.sex,
+        blood_type = patient_data.blood_type
     )
     db.add(db_patient)
     try:
@@ -37,3 +37,11 @@ async def create_patient(patient_data: schemas.PatientCreate, db: Session = Depe
         db.rollback()
         raise HTTPException(status_code=400, detail=f"Error creating patient: {e}")
     return db_patient
+
+@app.get("/patients/{patient_id}", response_model = schemas.Patient)
+async def get_patient(patient_id: int, db: Session = Depends(get_session)):
+    db_patient = db.query(models.Patient).get(patient_id)
+    if not db_patient:
+        raise HTTPException(status_code = 404, detail = f"Patient with id {id} not found!")
+    return db_patient
+
