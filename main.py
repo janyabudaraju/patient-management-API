5796353452289c614d13c274bc512090a5c6e4bd
Janya Budaraju <budaraju.janya@gmail.com>
2024-02-03 16:36:59 -0500

    finishing prototype pre-testing

--- main.py
+++ main.py
@@ -16,11 +16,11 @@ Base.metadata.create_all(engine)
 app = FastAPI()
 
 @app.get("/")
-async def root():
+def root():
     return {"message": "Janya's REST API prototype!!"}
 
 @app.post("/patients/", response_model = schemas.Patient, status_code = status.HTTP_201_CREATED)
-async def create_patient(patient_data: schemas.PatientCreate, db: Session = Depends(get_session)):
+def create_patient(patient_data: schemas.PatientCreate, db: Session = Depends(get_session)):
     db_patient = models.Patient(
         first_name = patient_data.first_name,
         middle_name = patient_data.middle_name,
@@ -39,19 +39,33 @@ async def create_patient(patient_data: schemas.PatientCreate, db: Session = Depe
     return db_patient
 
 @app.get("/patients/{patient_id}", response_model = schemas.Patient)
-async def get_patient(patient_id: int, db: Session = Depends(get_session)):
+def get_patient(patient_id: int, db: Session = Depends(get_session)):
     db_patient = db.query(models.Patient).get(patient_id)
     if not db_patient:
         raise HTTPException(status_code = 404, detail = f"Patient with id {id} not found!")
     return db_patient
 
+@app.delete("/patients/{patient_id}", response_model = schemas.Patient)
+def delete_patient(patient_id: int, db: Session = Depends(get_session)):
+    db_patient = db.query(models.Patient).get(patient_id)
+    if not db_patient:
+        raise HTTPException(status_code = 404, detail = f"Patient with id {patient_id} not found!")
+    db.delete(db_patient)
+    db.commit()
+    return db_patient
 
-
-@app.delete("/patients/{patient_id}")
-async def delete_todo(patient_id: int, db: Session = Depends(get_session)):
+@app.put("/patients/{patient_id}", response_model=schemas.Patient)
+def update_patient(patient_id: int, updated: schemas.PatientUpdate, db: Session = Depends(get_session)):
     db_patient = db.query(models.Patient).get(patient_id)
     if not db_patient:
         raise HTTPException(status_code = 404, detail = f"Patient with id {id} not found!")
-    db.delete(db_patient)
-    db.commit()
+    new_patient_data = updated.model_dump(exclude_unset=True)
+    for key, value in new_patient_data.items():
+        setattr(db_patient, key, value)
+    try:
+        db.commit()
+        db.refresh(db_patient)
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(status_code=400, detail=f"Error updating patient: {e}")
     return db_patient
